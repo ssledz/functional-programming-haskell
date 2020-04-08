ee8f56e6c3bdad1bf2f19e075686e9165cda2d9e
Sławomir Śledź <slawomir.sledz@iiit.pl>
2020-04-08 18:21:18 +0200

    Write a Spelling Book Generator

--- week3/spelling-book-generator.hs
+++ week3/spelling-book-generator.hs
@@ -1,2 +1,21 @@
+module Main where
+
+import System.Environment
+
+main :: IO ()
 main = do
-  print "Hello World!"
+  args <- getArgs
+  putStrLn (speller args)
+
+speller :: [[Char]] -> [Char]
+
+speller [] = ""
+speller [word] = spell word
+speller words =
+  let x = last words
+      xs = init words
+      ys = foldr (\w acc -> ", " ++ w ++ acc) "" (map spell xs)
+  in (tail (tail ys)) ++ ", and " ++ (spell x)
+
+spell :: [Char] -> [Char]
+spell word = (head word):(" is for " ++ word)
