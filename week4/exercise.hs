78520205a55bf6ef75d239148488114813222b7d
Sławomir Śledź <slawomir.sledz@gmail.com>
2020-04-13 14:09:04 +0200

    some playing with parsec

--- week4/exercise.hs
+++ week4/exercise.hs
@@ -47,4 +47,49 @@ fmap inc (Just 1)
 fmap inc $ Just 2
 fmap inc Nothing
 
+"Practical parsing"
+
+let {
+  sayHello name = do
+    let str = "Hello " ++ name
+    print str
+    return $ "ret: " ++ str
+}
+
+sayHello "John"
+
+import Text.ParserCombinators.Parsec
+
+data Tag = MkTag String deriving Show
+
+identifier = many (noneOf ['<', '>']) :: Parser String
+parseTag = do { char '<'; x <- identifier; char '>'; return (MkTag x) } :: Parser Tag
+
+parseTest parseTag "<div>"
+
+parseTest (char 'c') "const"
+
+
+parseDiv = do { string "<div>"; return (MkTag "div") } :: Parser Tag
+
+parseTest parseDiv "<div>Hello World</div>"
+
+letterOrDigit = do { x <- letter <|> digit; return x} :: Parser Char
+
+parseTest letterOrDigit "1"
+parseTest letterOrDigit "a"
+
+bagOrBog = try (string "bag") <|> string "bog" :: Parser String
+parseTest bagOrBog "bag"
+parseTest bagOrBog "bog"
+
+
+varname = do { x <- letter; xs <- many (letter <|> digit); return (x:xs) } :: Parser String
+parseTest varname "ab123"
+--parseTest varname "1b123"
+
+Just 1 >> Just 2
+Just 1 >>= \x -> Just (x + 1)
+
+
 
